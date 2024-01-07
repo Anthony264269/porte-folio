ab3a873d1c217846840aeb4afc70ed611660fe88
Anthony264269 <tony264269@gmail.com>
2024-01-07 15:20:15 +0100

    navbar

--- main.js
+++ main.js
@@ -1,28 +1,30 @@
-let carousel = document.querySelector('.carousel'),
-  figure = carousel.querySelector('figure'),
-  nav = carousel.querySelector('nav'),
-  numImages = figure.childElementCount,
-  theta = 1.5 * Math.PI / numImages,
-  currImage = 0;
+// let carousel = document.querySelector('.carousel'),
+//   figure = carousel.querySelector('figure'),
+//   nav = carousel.querySelector('nav'),
+//   numImages = figure.childElementCount,
+//   theta = 1.5 * Math.PI / numImages,
+//   currImage = 0;
 
   
-nav.addEventListener('click', onClick, true);
 
-let images = document.querySelectorAll('.plan');
 
-images.forEach(function (image) {
-  image.addEventListener('click', function () {
-    window.location.href = "https://openclassrooms.com/fr/";
 
-  });
-});
+// let images = document.getElementById('#mountain');
+// console.log(images);
+// images.forEach(function (image) {
+//   image.addEventListener('click', function () {
+//     window.location.href = "https://openclassrooms.com/fr/";
 
-// // Sélectionnez toutes les images avec la classe 'plan'
-// let image = document.querySelectorAll('.plan');
-
-// // Attachez un gestionnaire d'événements à chaque image
-// images.forEach(function(image) {
-//     image.onclick = function() {
-//         window.location.href = "https://openclassrooms.com/fr/";
-//     };
+//   });
 // });
+
+// Sélectionnez toutes les images avec la classe 'plan'
+let images = document.querySelectorAll('.plan');
+console.log(images);
+// Attachez un gestionnaire d'événements à chaque image
+images.forEach(function(image) {
+    image.onclick = function() {
+      
+        window.location.href = "https://openclassrooms.com/fr/";
+    };
+});
